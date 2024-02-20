ef8b8ae87dbd6d6bdeece1bb8f4c7cd4844f1bb8
Antoha63 <44719135+Antoha63@users.noreply.github.com>
2024-02-20 16:57:06 +0400

    Delete Desktop/Anton/generator directory

--- Desktop/Anton/generator/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname));
-
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
