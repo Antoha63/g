1d63e98b727aeaf9ecf8b351e31dc686a927107f
Antoha63 <44719135+Antoha63@users.noreply.github.com>
2024-03-11 20:34:26 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,18 @@ const passwordNavLink = document.getElementById("password-nav-link"),
     uuidNavLink = document.getElementById("uuid-nav-link"),
     numberNavLink = document.getElementById("number-nav-link"),
 
+    mainDiv = document.getElementById("main-div"),
+
     passwordMainDiv = document.getElementById("password-main-div"),
     qrCodeMainDiv = document.getElementById("qr-code-main-div"),
     uuidMainDiv = document.getElementById("uuid-main-div"),
     numberMainDiv = document.getElementById("number-main-div");
 
+const icon = document.querySelector('.icon-1');
+const arrow = document.querySelector('.arrow-1');;
+
 passwordNavLink.addEventListener("click", () => {
+    mainDiv.hidden = true;
     passwordMainDiv.hidden = false;
     qrCodeMainDiv.hidden = true;
     uuidMainDiv.hidden = true;
@@ -16,6 +22,7 @@ passwordNavLink.addEventListener("click", () => {
 });
 
 qrCodeNavLink.addEventListener("click", () => {
+    mainDiv.hidden = true;
     passwordMainDiv.hidden = true;
     qrCodeMainDiv.hidden = false;
     uuidMainDiv.hidden = true;
@@ -23,6 +30,7 @@ qrCodeNavLink.addEventListener("click", () => {
 });
 
 uuidNavLink.addEventListener("click", () => {
+    mainDiv.hidden = true;
     passwordMainDiv.hidden = true;
     qrCodeMainDiv.hidden = true;
     uuidMainDiv.hidden = false;
@@ -30,6 +38,7 @@ uuidNavLink.addEventListener("click", () => {
 });
 
 numberNavLink.addEventListener("click", () => {
+    mainDiv.hidden = true;
     passwordMainDiv.hidden = true;
     qrCodeMainDiv.hidden = true;
     uuidMainDiv.hidden = true;
@@ -39,24 +48,43 @@ numberNavLink.addEventListener("click", () => {
 window.addEventListener("load", () => {
     let href = window.location.href;
     if (href.indexOf("/password") > -1) {
+        mainDiv.hidden = true;
         passwordMainDiv.hidden = false;
         qrCodeMainDiv.hidden = true;
         uuidMainDiv.hidden = true;
         numberMainDiv.hidden = true;
     } else if (href.indexOf("/qr-code") > -1) {
+        mainDiv.hidden = true;
         passwordMainDiv.hidden = true;
         qrCodeMainDiv.hidden = false;
         uuidMainDiv.hidden = true;
         numberMainDiv.hidden = true;
     } else if (href.indexOf("/uuid") > -1) {
+        mainDiv.hidden = true;
         passwordMainDiv.hidden = true;
         qrCodeMainDiv.hidden = true;
         uuidMainDiv.hidden = false;
         numberMainDiv.hidden = true;
     } else if (href.indexOf("/number") > -1) {
+        mainDiv.hidden = true;
         passwordMainDiv.hidden = true;
         qrCodeMainDiv.hidden = true;
         uuidMainDiv.hidden = true;
         numberMainDiv.hidden = false;
+    } else {
+        mainDiv.hidden = false;
+        passwordMainDiv.hidden = true;
+        qrCodeMainDiv.hidden = true;
+        uuidMainDiv.hidden = true;
+        numberMainDiv.hidden = true;
     }
+
+    arrow.animate([
+        { left: '0' },
+        { left: '10px' },
+        { left: '0' }
+    ], {
+        duration: 700,
+        iterations: Infinity
+    });
 });
